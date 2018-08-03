9bce062809054e7314f038dfc921769dc539506c
Lucas Almeida <lucasga@dcc.ufba.br>
2018-08-03 12:11:47 -0300

    Starting to get data from API

--- src/Components/MovieBox/index.js
+++ src/Components/MovieBox/index.js
@@ -6,9 +6,11 @@ class MovieBox extends Component {
   render() {
     return (
         <div className="movie-box">
+          {
             <CardMovie
-                img="https://is5-ssl.mzstatic.com/image/thumb/Video118/v4/e6/bd/74/e6bd7401-8f27-e30b-e384-230b33944465/source/1200x630bb.jpg"  
+              img="https://is5-ssl.mzstatic.com/image/thumb/Video118/v4/e6/bd/74/e6bd7401-8f27-e30b-e384-230b33944465/source/1200x630bb.jpg"  
             />
+          }
         </div>
     );
   }
